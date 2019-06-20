9885678744739f13f517995e441b6c8e2de6986c
1213William <1950423854@qq.com>
2019-06-20 11:06:02 +0800

    add balance

--- shop.py
+++ shop.py
@@ -1,15 +1,20 @@
+import goods
+import os
+
+
 class Shop:
 
     def __init__(self):
         self.current_name = []
+        self.shop_goods_name = goods.books
 
     # 注册+充值
     def register(self):
         flag = True
         while flag:
-            uname = input('请输入用户名').strip()
-            pwd1 = input('请输入密码').strip()
-            pwd2 = input('请再次输入密码').strip()
+            uname = input('请输入用户名:').strip()
+            pwd1 = input('请输入密码:').strip()
+            pwd2 = input('请再次输入密码:').strip()
             if not uname.isalpha():
                 print('用户名必须是全英文的')
                 continue
@@ -19,13 +24,14 @@ class Shop:
             if not pwd2.isdigit():
                 print('密码必须是全数字的')
                 continue
-            balance = input('请输入你要充值的金额').strip()
-            if not balance.isdigit():
-                print('输入的金额必须是数字！！！')
-                continue
-            with open('username', 'a', encoding='utf-8') as f:
-                f.write('%s,%s,%s\n' % (uname, pwd2, balance))
-                flag = False
+            while flag:
+                balance = input('请输入你要充值的金额:').strip()
+                if not balance.isdigit():
+                    print('输入的金额必须是数字！！！')
+                    continue
+                with open('username', 'a', encoding='utf-8') as f:
+                    f.write('%s,%s,%s\n' % (uname, pwd2, balance))
+                    flag = False
 
     # 登陆
     def login(self):
@@ -48,14 +54,56 @@ class Shop:
                 for line in f:
                     l = line.strip().split(',')
                     if self.current_name[0] == l[0]:
-                        print('当前余额为：%s' % l[2])
+                        print('当前余额为：%s' % l[2])  # <class 'str'>
+                        return l[2]
                 # print(self.current_name[0])
         else:
             print('请先登录...')
 
     # 商品列表
-    def goods(self):
-        print('this is goods')
+    def buy_shop_goods(self):
+        flag = True
+        if self.current_name:
+            for i in self.shop_goods_name:
+                print('书名: %s' % (i['name']))
+            # print(self.shop_goods_name)
+            while flag:
+                b_name = input('输入书名查看详情:').strip()
+                for i in self.shop_goods_name:
+                    if b_name == i['name']:
+                        print(i)
+                        print('是否进行购买:y/n')
+                        choice = input('>>').strip()
+                        if choice == 'n':
+                            break
+                        elif choice == 'y':
+                            with open('username', 'r', encoding='utf-8') as fr,\
+                                open('new_username', 'w', encoding='utf-8') as fw:
+                                for line in fr:
+                                    l = line.strip().split(',')
+                                    if self.current_name[0] == l[0]:
+                                        new_balance = float(l[2]) - float(i['price'])
+                                        l[2] = round(new_balance, 1)
+                                        print('购买成功')
+                                    l = [str(i) for i in l]
+                                    l = ','.join(l)
+                                    fw.write(l+'\n')
+                            os.remove('username')
+                            os.rename('new_username', 'username')
+                                        # print(new_balance)
+
+                                    # break
+                            # new_balance = balance - int(i['price'])
+                            # print(new_balance)
+                        # elif choice == 'q':
+                        #     break
+                            break
+                        else:
+                            print('请理性输入')
+                else:
+                    print('请输入正确的书名...')
+        else:
+            print('请先登录...')
 
     def main(self):
         while 1:
@@ -73,7 +121,10 @@ class Shop:
             elif choice == '2':
                 self.login()
             elif choice == '3':
-                self.balance()
+                 self.balance()
+            elif choice == '4':
+                self.buy_shop_goods()
+
             else:
                 print('输入错误请重新输入...')
 
