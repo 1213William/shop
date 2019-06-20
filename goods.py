43c28379fefa5ec06b69df5dc53f4279fe9a3c54
1213William <1950423854@qq.com>
2019-06-20 11:06:19 +0800

    add books

--- goods.py
+++ goods.py
@@ -1,2 +1,206 @@
-feiji:1
-大炮:2
+books = [
+    {
+        "name": "a",
+        "author": "[美] 埃里克·马瑟斯",
+        "price": "9.1",
+        "date": "2016-7-1"
+    },
+    {
+        "name": "Python基础教程（第3版）",
+        "author": "[挪] 海特兰德",
+        "price": "8.0",
+        "date": "2018-2-1"
+    },
+    {
+        "name": "Python编程快速上手",
+        "author": "[美] 思维加特",
+        "price": "9.0",
+        "date": "2016-7-1"
+    },
+    {
+        "name": "笨办法学Python3",
+        "author": "[美] 泽德",
+        "price": "8.4",
+        "date": "2018-6-1"
+    },
+    {
+        "name": "流畅的Python",
+        "author": "[巴西] 拉马略",
+        "price": "9.4",
+        "date": "2017-5-15"
+    },
+    {
+        "name": "Python Cookbook 中文版",
+        "author": "[美] 比斯利",
+        "price": "9.2",
+        "date": "2015-5-1"
+    },
+    {
+        "name": "Effective Python",
+        "author": "[美] 斯拉特金",
+        "price": "9.0",
+        "date": "2016-1-18"
+    },
+    {
+        "name": "Python编程之美",
+        "author": "[美] 肯尼思·赖茨 Kenneth Reitz",
+        "price": "8.4",
+        "date": "2018-8-1"
+    },
+    {
+        "name": "Python高性能编程",
+        "author": "[美] 戈雷利克",
+        "price": "7.2",
+        "date": "2017-7-1"
+    },
+    {
+        "name": "Python网络爬虫权威指南（第2版）",
+        "author": "[美] 米切尔",
+        "price": "新书",
+        "date": "2019-4-1"
+    },
+    {
+        "name": "Python3网络爬虫开发实战",
+        "author": "崔庆才",
+        "price": "9.0",
+        "date": "2018-4-1"
+    },
+    {
+        "name": "利用Python进行数据分析（第2版）",
+        "author": "[美] 麦金尼",
+        "price": "8.5",
+        "date": "2018-7-30"
+    },
+    {
+        "name": "对比Excel，轻松学习Python数据分析",
+        "author": "张俊红",
+        "price": "7.8",
+        "date": "2019-2-1"
+    },
+    {
+        "name": "Python数据分析与挖掘实战",
+        "author": "张良均",
+        "price": "7.8",
+        "date": "2016-1-1"
+    },
+    {
+        "name": "统计学习方法（第2版）",
+        "author": "李航",
+        "price": "9.5",
+        "date": "2018-5-5"
+    },
+    {
+        "name": "白话深度学习与TensorFlow",
+        "author": "高扬",
+        "price": "7.1",
+        "date": "2017-7-31"
+    },
+    {
+        "name": "机器学习",
+        "author": "周志华",
+        "price": "8.7",
+        "date": "2016-1-1"
+    },
+    {
+        "name": "Python机器学习基础教程",
+        "author": "[德] 穆勒",
+        "price": "8.2",
+        "date": "2018-1-1"
+    },
+    {
+        "name": "Python深度学习",
+        "author": "[美] 肖莱",
+        "price": "9.6",
+        "date": "2018-8-1"
+    },
+    {
+        "name": "Python神经网络编程",
+        "author": "[英] 拉希德",
+        "price": "9.0",
+        "date": "2018-4-1"
+    },
+    {
+        "name": "你不知道的JavaScript",
+        "author": "[美] 辛普森",
+        "price": "9.3",
+        "date": "2018-1-1"
+    },
+    {
+        "name": "深入浅出Vue.js",
+        "author": "刘博文",
+        "price": "新书",
+        "date": "2018-3-1"
+    },
+    {
+        "name": "Django企业开发实战",
+        "author": "胡阳",
+        "price": "7.7",
+        "date": "2019-2-1"
+    },
+    {
+        "name": "Flask Web开发（第2版）",
+        "author": "[美] 格雷贝格",
+        "price": "9.4",
+        "date": "2018-8-1"
+    },
+    {
+        "name": "深入浅出Docker",
+        "author": "[英] 波尔顿",
+        "price": "8.4",
+        "date": "2019-4-1"
+    },
+    {
+        "name": "深入理解Kafka",
+        "author": "朱忠华",
+        "price": "新书",
+        "date": "2019-1-1"
+    },
+    {
+        "name": "Kubernetes权威指南（第4版）",
+        "author": "龚正",
+        "price": "新书",
+        "date": "2018-5-1"
+    },
+    {
+        "name": "集体智慧编程",
+        "author": "西格兰",
+        "price": "9.0",
+        "date": "2015-3-1"
+    },
+    {
+        "name": "程序员的数学",
+        "author": "[日] 结城浩",
+        "price": "8.6",
+        "date": "2016-6-25"
+    },
+    {
+        "name": "程序员修炼之道",
+        "author": "[美] 亨特",
+        "price": "8.6",
+        "date": "2011-1-1"
+    },
+    {
+        "name": "代码整洁之道",
+        "author": "[美] 罗伯特",
+        "price": "8.6",
+        "date": "2016-9-1"
+    },
+    {
+        "name": "持续交付2.0",
+        "author": "乔梁",
+        "price": "9.4",
+        "date": "2019-1-1"
+    },
+    {
+        "name": "鸟哥的Linux私房菜（第4版）",
+        "author": "鸟哥",
+        "price": "8.0",
+        "date": "2018-11-1"
+    },
+    {
+        "name": "颈椎病康复指南",
+        "author": "陈选宁",
+        "price": "8.6",
+        "date": "2012-4-1",
+    },
+]
